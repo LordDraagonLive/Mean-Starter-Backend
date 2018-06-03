d8d75814a907b9744bf76f34a3b7949f8d274686
Buddhi Adhikari <buddhiadikari@live.com>
2018-06-03 11:56:06 +0100

    modified employeeCtrl js

--- controllers/employeeCtrl.js
+++ controllers/employeeCtrl.js
@@ -47,7 +47,6 @@ router.put('/:id',(req,res) =>{
     if(!ObjectId.isValid(req.params.id))
         return res.status(400).send(`No record with given id : ${req.params.id}`);
 
-    
     var emp = {
         name:req.body.name,
         position:req.body.position,
