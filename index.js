cab3b99babad1457eeb8860611c23a52cebc7dfe
Buddhi Adhikari <buddhiadikari@live.com>
2018-06-03 19:28:45 +0100

    CORS intergration

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+// A middleware pkg whcih allows restricted resources (e.g. fonts) on a 
+// web page to be requested from another domain outside the domain from which the first resource was served.
+// CORS => Cross-origin resource sharing
+const cors = require('cors');
 
 const { mongoose } = require('./db.js');
 var employeeCtrl = require('./controllers/employeeCtrl.js');
 
 var app = express();
 app.use(bodyParser.json());
+app.use(cors({ origin: 'http://localhost:4200'}));
 
 app.listen(3000,() => console.log('Server started at port : 3000'));
 
